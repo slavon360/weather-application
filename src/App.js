489ef1c9dd42634802d2e3f0ab127fa31373a730
viacheslav liakh <slyakh@verasoftlabs.eu>
2019-02-26 11:52:04 +0200

    edits in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import Weather from './containers/Weather/Weather';
+import Layer from './components/Layer/Layer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Provider store={store}>
+        <Layer>
+          <Weather />
+        </Layer>
+      </Provider>
     );
   }
 }
